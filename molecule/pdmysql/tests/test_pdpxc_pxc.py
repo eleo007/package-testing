54e5a0993b136ff659ca9f45afe7e988fd57acdb
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2022-12-22 14:43:09 +0200

    Check running service

--- molecule/pdmysql/tests/test_pdpxc_pxc.py
+++ molecule/pdmysql/tests/test_pdpxc_pxc.py
@@ -109,6 +109,10 @@ def test_binary_version(host):
         assert result.rc == 0, result.stderr
         assert VERSION in result.stdout, result.stdout
 
+def test_mysql_running(host):
+    cmd = 'ps auxww| grep -v grep  | grep "mysql"'
+    result = host.run(cmd)
+    assert result.stdout in 'smth', result.stdout
 
 @pytest.mark.parametrize('component', ['@@INNODB_VERSION', '@@VERSION'])
 def test_mysql_version(host, component):
