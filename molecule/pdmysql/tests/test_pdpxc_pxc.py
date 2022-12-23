cd326070586f95fa60fe1de702a216b7f8d9e250
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2022-12-23 12:47:45 +0200

    Check release

--- molecule/pdmysql/tests/test_pdpxc_pxc.py
+++ molecule/pdmysql/tests/test_pdpxc_pxc.py
@@ -70,25 +70,6 @@ COMPONENTS = ['component_validate_password', 'component_log_sink_syseventlog',
 VERSION = os.environ['VERSION']
 
 
-@pytest.mark.parametrize("package", DEBPACKAGES)
-def test_check_deb_package(host, package):
-    dist = host.system_info.distribution
-    if dist.lower() in RHEL_DISTS:
-        pytest.skip("This test only for Debian based platforms")
-    pkg = host.package(package)
-    assert pkg.is_installed
-    assert VERSION in pkg.version, pkg.version
-
-
-@pytest.mark.parametrize("package", RPMPACKAGES)
-def test_check_rpm_package(host, package):
-    dist = host.system_info.distribution
-    if dist.lower() in DEB_DISTS:
-        pytest.skip("This test only for RHEL based platforms")
-    pkg = host.package(package)
-    assert pkg.is_installed
-    assert VERSION in pkg.version, pkg.version
-
 @pytest.mark.parametrize("package", EXTRA_RPMPACKAGE)
 def test_check_shared_package(host, package):
     dist = host.system_info.distribution
@@ -97,62 +78,5 @@ def test_check_shared_package(host, package):
         pytest.skip("This test only for RHEL based platforms")
     if dist.lower() in RHEL_DISTS and release == '9':
         pytest.skip("This test is for RHEL based platforms except RHEL 9")
-    pkg = host.package(package)
-    assert pkg.is_installed
-    assert VERSION in pkg.version, (pkg.version, release)
+    assert release == '9', release
 
-
-def test_binary_version(host):
-    with host.sudo("root"):
-        cmd = "mysql --version"
-        result = host.run(cmd)
-        assert result.rc == 0, result.stderr
-        assert VERSION in result.stdout, result.stdout
-
-
-@pytest.mark.parametrize('component', ['@@INNODB_VERSION', '@@VERSION'])
-def test_mysql_version(host, component):
-    with host.sudo("root"):
-        cmd = "mysql -e \"SELECT {}; \"| grep -c \"{}\"".format(component, VERSION)
-        result = host.run(cmd)
-        version = host.check_output("mysql -e \"SELECT {}; \"".format(component))
-        assert result.rc == 0, version
-        assert int(result.stdout) == 1, result.stdout
-
-
-def test_version_commnet(host):
-    with host.sudo("root"):
-        cmd = "mysql -e \"SELECT @@VERSION_COMMENT;\""
-        result = host.run(cmd)
-        print(result.stdout)
-        assert result.rc == 0, result.stdout
-
-
-def test_wresp_version(host):
-    with host.sudo("root"):
-        cmd = "mysql -e \"SHOW STATUS LIKE 'wsrep_provider_version';\""
-        result = host.run(cmd)
-        print(result.stdout)
-        assert result.rc == 0, result.stdout
-
-
-@pytest.mark.parametrize('plugin_command', PLUGIN_COMMANDS)
-def test_plugins(host, plugin_command):
-    with host.sudo("root"):
-        result = host.run(plugin_command)
-        assert result.rc == 0, (result.stderr, result.stdout)
-
-
-@pytest.mark.parametrize("component", COMPONENTS)
-def test_components(component, host):
-    with host.sudo("root"):
-        cmd = 'mysql -Ns -e "select count(*) from mysql.component where component_urn=\"file://{}\";"'.format(component)
-        check_component = host.run(cmd)
-        if check_component.rc == 0:
-            inst_cmd = 'mysql -e "INSTALL COMPONENT \"file://{}\";"'.format(component)
-            inst_res = host.run(inst_cmd)
-            assert inst_res.rc == 0, inst_res.stderr
-        check_cmd = 'mysql -Ns -e "select count(*) from mysql.component where component_urn=\"file://{}\";"'.format(
-            component)
-        check_result = host.run(check_cmd)
-        assert check_result.rc == 1, (check_result.rc, check_result.stderr, check_result.stdout)
