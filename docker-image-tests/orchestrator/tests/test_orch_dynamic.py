666507693810c2276282ae955d15fd4e7017b80d
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2023-05-01 18:29:02 +0300

    Use replica and osurce instead of master and slave, add timeout

--- docker-image-tests/orchestrator/tests/test_orch_dynamic.py
+++ docker-image-tests/orchestrator/tests/test_orch_dynamic.py
@@ -8,7 +8,7 @@ from settings import *
 
 orch_container_name = 'orchestartor-docker-test-dynamic'
 ps_container_name = 'ps-docker'
-master_ps_container_name = 'ps-docker-master'
+source_ps_container_name = 'ps-docker-source'
 replica_ps_container_name = 'ps-docker-replica'
 network_name = 'orchestrator'
 
@@ -18,16 +18,17 @@ def host():
     orch_docker_id = subprocess.check_output(
         ['docker', 'run', '--name', orch_container_name, '-d', '--network', network_name, docker_image ]).decode().strip()
     time.sleep(20)
-    master_ps_docker_id = subprocess.check_output(
-        ['docker', 'run', '--name', master_ps_container_name, '-e', 'MYSQL_ROOT_PASSWORD=secret', '-d', '--network', network_name, ps_docker_image,
-        '--log-error-verbosity=3', '--report_host="'+master_ps_container_name+'"', '--max-allowed-packet=134217728']).decode().strip()
+    source_ps_docker_id = subprocess.check_output(
+        ['docker', 'run', '--name', source_ps_container_name, '-e', 'MYSQL_ROOT_PASSWORD=secret', '-d', '--network', network_name, ps_docker_image,
+        '--log-error-verbosity=3', '--report_host="'+source_ps_container_name+'"', '--max-allowed-packet=134217728']).decode().strip()
     time.sleep(20)
-    slave_ps_docker_id = subprocess.check_output(
+    replica_ps_docker_id = subprocess.check_output(
         ['docker', 'run', '--name', replica_ps_container_name, '-e', 'MYSQL_ROOT_PASSWORD=secret', '-d', '--network', network_name, ps_docker_image, 
-        '--log-error-verbosity=3', '--report_host="'+replica_ps_container_name+'"', '--max-allowed-packet=134217728']).decode().strip()
-    subprocess.check_call(['docker', 'exec', master_ps_docker_id, 'mysql', '-uroot', '-psecret', '-e', 'CREATE USER \'repl\'@\'%\' IDENTIFIED WITH mysql_native_password BY \'slavepass\'; GRANT REPLICATION SLAVE ON *.* TO \'repl\'@\'%\';'])
-    subprocess.check_call(['docker', 'exec', slave_ps_docker_id, 'mysql', '-uroot', '-psecret', '-e', 'change master to master_host="'+master_ps_container_name+'",master_user="repl",master_password="slavepass",master_log_file="binlog.000002";show warnings;'])
-    subprocess.check_call(['docker', 'exec', slave_ps_docker_id, 'mysql', '-uroot', '-psecret', '-e', 'START SLAVE;'])
+        '--log-error-verbosity=3', '--report_host="'+replica_ps_container_name+'"', '--max-allowed-packet=134217728', '--server-id=2']).decode().strip()
+    time.sleep(20)
+    subprocess.check_call(['docker', 'exec', source_ps_docker_id, 'mysql', '-uroot', '-psecret', '-e', 'CREATE USER \'repl\'@\'%\' IDENTIFIED WITH mysql_native_password BY \'replicapass\'; GRANT REPLICATION SLAVE ON *.* TO \'repl\'@\'%\';'])
+    subprocess.check_call(['docker', 'exec', replica_ps_docker_id, 'mysql', '-uroot', '-psecret', '-e', 'CHANGE REPLICATION SOURCE to SOURCE_HOST="'+source_ps_container_name+'",SOURCE_USER="repl",SOURCE_PASSWORD="replicapass",SOURCE_LOG_FILE="binlog.000002";show warnings;'])
+    subprocess.check_call(['docker', 'exec', replica_ps_docker_id, 'mysql', '-uroot', '-psecret', '-e', 'START REPLICA;'])
     # yield testinfra.get_host("docker://root@" + orch_docker_id)
     # subprocess.check_call(['docker', 'rm', '-f', orch_docker_id])
 
