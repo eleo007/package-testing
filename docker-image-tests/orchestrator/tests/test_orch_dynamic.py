9ed343dc5a358b16d4d053cfe754b5161deb170f
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2023-05-01 16:53:36 +0300

    Fix comnds - add ''

--- docker-image-tests/orchestrator/tests/test_orch_dynamic.py
+++ docker-image-tests/orchestrator/tests/test_orch_dynamic.py
@@ -6,7 +6,7 @@ import json
 import time
 from settings import *
 
-orch_container_name = 'orchestartor-docker-test-static'
+orch_container_name = 'orchestartor-docker-test-dynamic'
 ps_container_name = 'ps-docker'
 network_name = 'orchestrator'
 
@@ -22,9 +22,9 @@ def host():
     time.sleep(20)
     slave_ps_docker_id = subprocess.check_output(
         ['docker', 'run', '--name', ps_container_name+'slave', '-d', '--network', network_name, ps_docker_image ]).decode().strip()
-    subprocess.check_call(['docker exec -ti', master_ps_docker_id, '\'mysql\' -uroot -psecret -vvv -e"CREATE USER \'repl\'@\'%\' IDENTIFIED WITH mysql_native_password BY \'slavepass\'; GRANT REPLICATION SLAVE ON *.* TO repl@\'%\';"'])
-    subprocess.check_call(['docker exec -ti', slave_ps_docker_id, 'mysql -uroot -psecret -e\'change master to master_host="'+ps_container_name+'master'+'",master_user="repl",master_password="slavepass",master_log_file="binlog.000002";show warnings;\''])
-    subprocess.check_call(['docker exec -ti ps8032_slave mysql -uroot -psecret -e\'START SLAVE;\''])
+    subprocess.check_call(['docker', 'exec', '-ti', master_ps_docker_id, '\'mysql\' -uroot -psecret -vvv -e"CREATE USER \'repl\'@\'%\' IDENTIFIED WITH mysql_native_password BY \'slavepass\'; GRANT REPLICATION SLAVE ON *.* TO repl@\'%\';"'])
+    subprocess.check_call(['docker', 'exec', '-ti', slave_ps_docker_id, 'mysql -uroot -psecret -e\'change master to master_host="'+ps_container_name+'master'+'",master_user="repl",master_password="slavepass",master_log_file="binlog.000002";show warnings;\''])
+    subprocess.check_call(['docker', 'exec', '-ti', 'ps8032_slave mysql -uroot -psecret -e\'START SLAVE;\''])
     # yield testinfra.get_host("docker://root@" + orch_docker_id)
     # subprocess.check_call(['docker', 'rm', '-f', orch_docker_id])
 
