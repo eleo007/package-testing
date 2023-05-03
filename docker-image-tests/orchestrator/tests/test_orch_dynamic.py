6f8f98cefd53b929d43918a71c038363a159cabb
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2023-05-03 20:52:46 +0300

    Use requests module

--- docker-image-tests/orchestrator/tests/test_orch_dynamic.py
+++ docker-image-tests/orchestrator/tests/test_orch_dynamic.py
@@ -6,14 +6,15 @@ import json
 import time
 from settings import *
 import os
+import requests
 
 orch_container_name = 'orchestartor-docker-test-dynamic'
 source_ps_container_name = 'ps-docker-source'
 replica_ps_container_name = 'ps-docker-replica'
 network_name = 'orchestrator'
-source_filters = (
-    (source_ps_container_name,'.Key.Hostname'),(ps_docker_tag, 'Version'),(replica_ps_container_name, 'SlaveHosts'),
-    ('true', 'IsLastCheckValid'),('true','IsUpToDate'),('6','SecondsSinceLastSeen.Int64'))
+source_state_values = (
+    ('Key.Hostname', source_ps_container_name),('Version', ps_docker_tag),('SlaveHosts',replica_ps_container_name),
+    ('IsLastCheckValid', 'true'),('IsUpToDate','true'),('SecondsSinceLastSeen.Int64','7'))
 
 @pytest.fixture(scope='module')
 def prepare():
@@ -34,25 +35,31 @@ def prepare():
     subprocess.check_call(['docker', 'exec', replica_ps_container_name, 'mysql', '-uroot', '-psecret', '-e', 'START REPLICA;'])
     subprocess.check_call(['docker', 'exec', source_ps_container_name, 'mysql', '-uroot', '-psecret', '-e', 'CREATE USER \'orchestrator\'@\'%\' IDENTIFIED  WITH mysql_native_password BY \'\'; GRANT SUPER, PROCESS, REPLICATION SLAVE, RELOAD ON *.* TO \'orchestrator\'@\'%\'; GRANT SELECT ON mysql.slave_master_info TO \'orchestrator\'@\'%\';'])
     source_ps_ip = subprocess.check_output(['docker', 'inspect', '-f' '"{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}"', source_ps_container_name]).decode().strip()
+    orchestrator_ip = subprocess.check_output(['docker', 'inspect', '-f' '"{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}"', orch_container_name]).decode().strip().replace('"','')
 #     # yield testinfra.get_host("docker://root@" + orch_docker_id)
 #     # subprocess.check_call(['docker', 'rm', '-f', orch_docker_id])
 # #curl "http://172.18.0.2:3000/api/discover/172.18.0.3/3306"| jq '.'
 
-def run_api_query (host, command, filter):
-    orchestrator_ip = subprocess.check_output(['docker', 'inspect', '-f' '"{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}"', orch_container_name]).decode().strip().replace('"','')
-    cmd = host.run('curl -s http://'+orchestrator_ip+':3000/api/'+command+'/'+source_ps_container_name+'/3306| jq -r \'.'+filter+'\'' )
-    assert cmd.succeeded
-    return cmd.stdout
+# def run_api_query (host, command, filter):
+#     orchestrator_ip = subprocess.check_output(['docker', 'inspect', '-f' '"{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}"', orch_container_name]).decode().strip().replace('"','')
+#     cmd = host.run('curl -s http://'+orchestrator_ip+':3000/api/'+command+'/'+source_ps_container_name+'/3306| jq -r \'.'+filter+'\'' )
+#     assert cmd.succeeded
+#     return cmd.stdout
 
 def test_discovery(host, prepare):
-    message = run_api_query(host,'discover', 'Message')
-    assert message == 'Instance discovered: ps-docker-source:3306\n', (message)
+    discover = requests.get('http://{}:3000/api/discover/{}/3306'.format(orchestrator_ip, source_ps_container_name))
+    discover_output = json.loads(discover.text)
+    assert discover_output['Message'] == 'Instance discovered: ps-docker-source:3306\n', (discover_output)
 
 #curl -s "http://172.18.0.2:3000/api/instance/ps-docker-source/3306"| jq .
-def test_source(host, prepare):
-    for value in source_filters:
-        message = run_api_query(host, 'instance', value[1])
-        assert value[0] in message, message
+# def test_source(host, prepare):
+#     cur_state=
+#     for value in source_state_values:
+#         state = run_api_query(host, 'instance', value[1])
+#         if value[0] != 'SecondsSinceLastSeen.Int64':
+#             assert value[1] in state, state
+#         else:
+#             assert state < value[1], (value, state)
 
 # curl -s "http://172.18.0.2:3000/api/instance/ps-docker-replica/3306"| jq .
 # def test_replica(host, prepare):
