e39b80f4a445f33932e7240cf10978f117707e96
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2023-05-10 22:25:38 +0300

    Use dict for values

--- docker-image-tests/orchestrator/tests/test_orch_dynamic.py
+++ docker-image-tests/orchestrator/tests/test_orch_dynamic.py
@@ -50,7 +50,11 @@ def prepare():
         subprocess.check_call(['docker', 'exec', source_ps_container, 'mysql', '-uroot', '-p'+ps_password+'', '-e', 'CREATE USER \'repl\'@\'%\' IDENTIFIED WITH mysql_native_password BY \'replicapass\'; GRANT REPLICATION SLAVE ON *.* TO \'repl\'@\'%\';'])
         subprocess.check_call(['docker', 'exec', replica_ps_container, 'mysql', '-uroot', '-p'+ps_password+'', '-e', 'CHANGE REPLICATION SOURCE to SOURCE_HOST=\''+source_ps_container+'\',SOURCE_USER=\'repl\',SOURCE_PASSWORD=\'replicapass\',SOURCE_LOG_FILE=\'binlog.000002\';START REPLICA;'])
         subprocess.check_call(['docker', 'exec', source_ps_container, 'mysql', '-uroot', '-p'+ps_password+'', '-e', 'CREATE USER \'orchestrator\'@\'%\' IDENTIFIED  WITH mysql_native_password BY \'\'; GRANT SUPER, PROCESS, REPLICATION SLAVE, RELOAD ON *.* TO \'orchestrator\'@\'%\'; GRANT SELECT ON mysql.slave_master_info TO \'orchestrator\'@\'%\';'])
-        #get orchestrator container IP
+        subprocess.check_call(['docker', 'exec', source_ps_container, 'mysql', '-uroot', '-p'+ps_password+'', '-e', \
+                            'CREATE USER \'sysbench\'@\'%\' IDENTIFIED  WITH mysql_native_password BY \'Test1234#\'; \
+                            GRANT ALL PRIVILEGES on *.* to \'sysbench\'@\'%\'; \
+                            CREATE DATABASE sbtest;'])
+            #get orchestrator container IP
         orchestrator_ip = subprocess.check_output(['docker', 'inspect', '-f' '"{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}"', orch_container]).decode().strip().replace('"','')
         return orchestrator_ip
 
@@ -74,21 +78,6 @@ def prepare():
 #     return replica_state
 
 # @pytest.fixture(scope='module')
-# def load_state(host):
-#     source_ps_ip = subprocess.check_output(['docker', 'inspect', '-f' '"{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}"', source_ps_container]).decode().strip()
-#     subprocess.check_call(['docker', 'exec', source_ps_container, 'mysql', '-uroot', '-p'+ps_password+'', '-e', \
-#                            'CREATE USER \'sysbench\'@\'%\' IDENTIFIED  WITH mysql_native_password BY \'Test1234#\'; \
-#                            GRANT ALL PRIVILEGES on *.* to \'sysbench\'@\'%\'; \
-#                            CREATE DATABASE sbtest;'])
-#     cmd='sysbench --tables=20 --table-size=10000 --threads=4 --rand-type=pareto --db-driver=mysql \
-#         --mysql-user=sysbench --mysql-password=Test1234# --mysql-host={} --mysql-port=3306 --mysql-db=sbtest --mysql-storage-engine=innodb \
-#         /usr/share/sysbench/oltp_read_write.lua prepare'.format(source_ps_ip)
-#     host.run(cmd)
-#     time.sleep(15)
-#     load_state=run_api_call('instance', replica_ps_container)
-#     return load_state
-
-# @pytest.fixture(scope='module')
 # def replica_stopped_state(host):
 #     time.sleep(2)
 #     subprocess.check_call(['docker', 'exec', replica_ps_container, 'mysql', '-uroot', '-psecret', '-e', 'STOP REPLICA;'])
@@ -100,11 +89,6 @@ def prepare():
 #     host.run(cmd)
 
 orchestrator_ip = prepare()
-
-# def run_api_call(command, ps_server):
-#     server_state = requests.get('http://{}:3000/api/{}/{}/3306'.format(orchestrator_ip, command, ps_server))
-#     parced_state = json.loads(server_state.text)
-#     return parced_state
 
 def receive_current_value(key_path, server_state):
     if len(key_path) == 2:
@@ -133,30 +117,39 @@ def test_source():
         print(current_attr_value)
         assert current_attr_value == value['expected_value'], value
 
-# def test_replica(replica_state, value, key1, key2):
-#     replica_state=run_api_call('instance', replica_ps_container)
-#     for value in replica_attr_reference:
-#         refence_value = value[0]
-#         current_value = receive_current_value(value[1], value[2], replica_state)
-#         assert current_value == refence_value, value
+def test_replica():
+    time.sleep(10)
+    r=requests.get('http://{}:3000/api/{}/{}/3306'.format(orchestrator_ip, 'instance', replica_ps_container))
+    source_state = json.loads(r.text)
+    assert r.status_code == 200
+    for value in replica_attr_reference:
+        current_attr_value = receive_current_value(value['key_path'], source_state)
+        print(current_attr_value)
+        assert current_attr_value == value['expected_value'], value
 
+def test_load(host):
+    source_ps_ip = subprocess.check_output(['docker', 'inspect', '-f' '"{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}"', source_ps_container]).decode().strip()
+    cmd='sysbench --tables=20 --table-size=10000 --threads=4 --rand-type=pareto --db-driver=mysql \
+        --mysql-user=sysbench --mysql-password=Test1234# --mysql-host={} --mysql-port=3306 --mysql-db=sbtest --mysql-storage-engine=innodb \
+        /usr/share/sysbench/oltp_read_write.lua prepare'.format(source_ps_ip)
+    host.run(cmd)
+    time.sleep(15)
+    r=requests.get('http://{}:3000/api/{}/{}/3306'.format(orchestrator_ip, 'instance', replica_ps_container))
+    source_state = json.loads(r.text)
+    assert r.status_code == 200
+    for value in replica_attr_reference:
+        current_attr_value = receive_current_value(value['key_path'], source_state)
+        print(current_attr_value)
+        assert current_attr_value == value['expected_value'], value
 
-# @pytest.mark.parametrize("value, key1, key2", replica_state_reference, ids=[f'{x[1]} {x[2]}' for x in replica_state_reference])
-# def test_load(load_state, value, key1, key2):
-#     if key2:
-#         if key1 == 'SecondsSinceLastSeen':
-#             assert value > load_state[key1][key2], value
-#         else: # All other cases.
-#             assert value == load_state[key1][key2], value
-#     else:
-#         assert value == load_state[key1], value
-
-# @pytest.mark.parametrize("value, key1, key2", replica_stopped_reference, ids=[f'{x[1]} {x[2]}' for x in replica_stopped_reference])
-# def test_replica_stopped(replica_stopped_state, value, key1, key2):
-#     if key2:
-#         if key1 == 'SecondsSinceLastSeen':
-#             assert value > replica_stopped_state[key1][key2], value
-#         else:
-#             assert value == replica_stopped_state[key1][key2], value
-#     else:
-#         assert value == replica_stopped_state[key1], value
+def test_replica_stopped():
+    time.sleep(2)
+    subprocess.check_call(['docker', 'exec', replica_ps_container, 'mysql', '-uroot', '-psecret', '-e', 'STOP REPLICA;'])
+    time.sleep(10)
+    r=requests.get('http://{}:3000/api/{}/{}/3306'.format(orchestrator_ip, 'instance', replica_ps_container))
+    source_state = json.loads(r.text)
+    assert r.status_code == 200
+    for value in replica_attr_reference:
+        current_attr_value = receive_current_value(value['key_path'], source_state)
+        print(current_attr_value)
+        assert current_attr_value == value['expected_value'], value
