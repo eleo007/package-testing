476690e5090e731e1544e81e1c292e049ac23a5e
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2023-05-10 22:45:18 +0300

    Use fixture

--- docker-image-tests/orchestrator/tests/test_orch_dynamic.py
+++ docker-image-tests/orchestrator/tests/test_orch_dynamic.py
@@ -35,7 +35,32 @@ replica_stopped_attr_reference = ({"key_path": ["Key", "Hostname"], "expected_va
                                   {"key_path": ["IsLastCheckValid"], "expected_value": True},
                                   {"key_path": ["IsUpToDate"], "expected_value": True},)
 
-def prepare():
+# def prepare():
+#         subprocess.check_call(['docker', 'network', 'create', network_name])
+#         #start orchestrator and PS containers
+#         subprocess.check_call(['docker', 'run', '--name', orch_container, '-d', '--network', network_name, docker_image ])
+#         time.sleep(10)
+#         subprocess.check_call(['docker', 'run', '--name', source_ps_container, '-e', 'MYSQL_ROOT_PASSWORD='+ps_password+'', '-d', '--network', network_name, ps_docker_image,
+#             '--log-error-verbosity=3', '--report_host='+source_ps_container, '--max-allowed-packet=134217728'])
+#         time.sleep(10)
+#         subprocess.check_call(['docker', 'run', '--name', replica_ps_container, '-e', 'MYSQL_ROOT_PASSWORD='+ps_password+'', '-d', '--network', network_name, ps_docker_image, 
+#             '--log-error-verbosity=3', '--report_host='+replica_ps_container, '--max-allowed-packet=134217728', '--server-id=2'])
+#         time.sleep(10)
+#         #setup replication between PS nodes
+#         subprocess.check_call(['docker', 'exec', source_ps_container, 'mysql', '-uroot', '-p'+ps_password+'', '-e', 'CREATE USER \'repl\'@\'%\' IDENTIFIED WITH mysql_native_password BY \'replicapass\'; GRANT REPLICATION SLAVE ON *.* TO \'repl\'@\'%\';'])
+#         subprocess.check_call(['docker', 'exec', replica_ps_container, 'mysql', '-uroot', '-p'+ps_password+'', '-e', 'CHANGE REPLICATION SOURCE to SOURCE_HOST=\''+source_ps_container+'\',SOURCE_USER=\'repl\',SOURCE_PASSWORD=\'replicapass\',SOURCE_LOG_FILE=\'binlog.000002\';START REPLICA;'])
+#         subprocess.check_call(['docker', 'exec', source_ps_container, 'mysql', '-uroot', '-p'+ps_password+'', '-e', 'CREATE USER \'orchestrator\'@\'%\' IDENTIFIED  WITH mysql_native_password BY \'\'; GRANT SUPER, PROCESS, REPLICATION SLAVE, RELOAD ON *.* TO \'orchestrator\'@\'%\'; GRANT SELECT ON mysql.slave_master_info TO \'orchestrator\'@\'%\';'])
+#         subprocess.check_call(['docker', 'exec', source_ps_container, 'mysql', '-uroot', '-p'+ps_password+'', '-e', \
+#                             'CREATE USER \'sysbench\'@\'%\' IDENTIFIED  WITH mysql_native_password BY \'Test1234#\'; \
+#                             GRANT ALL PRIVILEGES on *.* to \'sysbench\'@\'%\'; \
+#                             CREATE DATABASE sbtest;'])
+#             #get orchestrator container IP
+#         orchestrator_ip = subprocess.check_output(['docker', 'inspect', '-f' '"{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}"', orch_container]).decode().strip().replace('"','')
+#         return orchestrator_ip
+
+# orchestrator_ip = prepare()
+@pytest.fixture(scope='module', autouse=True)
+def orchestrator_ip(host):
         subprocess.check_call(['docker', 'network', 'create', network_name])
         #start orchestrator and PS containers
         subprocess.check_call(['docker', 'run', '--name', orch_container, '-d', '--network', network_name, docker_image ])
@@ -56,39 +81,9 @@ def prepare():
                             CREATE DATABASE sbtest;'])
             #get orchestrator container IP
         orchestrator_ip = subprocess.check_output(['docker', 'inspect', '-f' '"{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}"', orch_container]).decode().strip().replace('"','')
-        return orchestrator_ip
-
-
-
-# @pytest.fixture(scope='module')
-# def discover_state():
-#     discover_state=run_api_call('discover', source_ps_container)
-#     return discover_state
-
-# @pytest.fixture(scope='module')
-# def source_state():
-#     source_state=run_api_call('instance', source_ps_container)
-#     return source_state
-
-# @pytest.fixture(scope='module')
-# def replica_state():
-#     time.sleep(10)
-#     replica_state=run_api_call('instance', replica_ps_container)
-#     print('this is one run')
-#     return replica_state
-
-# @pytest.fixture(scope='module')
-# def replica_stopped_state(host):
-#     time.sleep(2)
-#     subprocess.check_call(['docker', 'exec', replica_ps_container, 'mysql', '-uroot', '-psecret', '-e', 'STOP REPLICA;'])
-#     time.sleep(10)
-#     replica_stopped_state=run_api_call('instance', replica_ps_container)
-#     print('this is one run')
-#     yield replica_stopped_state
-#     cmd='docker rm -f $(docker ps -a -q) || true && docker network rm {} || true'.format(network_name)
-#     host.run(cmd)
-
-orchestrator_ip = prepare()
+        yield orchestrator_ip
+        cmd='docker rm -f $(docker ps -a -q) || true && docker network rm {} || true'.format(network_name)
+        host.run(cmd)
 
 def receive_current_value(key_path, server_state):
     if len(key_path) == 2:
@@ -146,7 +141,7 @@ def test_replica_stopped():
     time.sleep(2)
     subprocess.check_call(['docker', 'exec', replica_ps_container, 'mysql', '-uroot', '-psecret', '-e', 'STOP REPLICA;'])
     time.sleep(10)
-    r=requests.get('http://{}:3000/api/{}/{}/3306'.format(orchestrator_ip, 'instance', replica_ps_container))
+    r=requests.get('http://{}:3000/api/{}/{}/3306'.format(orchestrator_ip, 'instance', replica_stopped_attr_reference))
     source_state = json.loads(r.text)
     assert r.status_code == 200
     for value in replica_attr_reference:
