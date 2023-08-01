3c287887cfac0da5858cecd235378eb1455d4580
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2023-08-01 17:30:39 +0300

    Use httpchk

--- molecule/pdmysql/haproxy/molecule/tests/test_haproxy.py
+++ molecule/pdmysql/haproxy/molecule/tests/test_haproxy.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import pytest
 import testinfra.utils.ansible_runner
@@ -6,16 +7,22 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-VERSION = os.getenv("PROXYSQL_VERSION")
+VERSION = os.getenv("VERSION")
 
 
 @pytest.fixture
 def create_user(host):
     with host.sudo("root"):
-        cmd = "mysql -e \"CREATE USER 'haproxy_user'@'%' IDENTIFIED WITH mysql_native_password by '$3Kr$t';\""
+        cmd = "mysql -e \"CREATE USER 'clustercheckuser'@'%' IDENTIFIED WITH mysql_native_password by 'clustercheckpassword!';GRANT PROCESS ON *.* TO 'clustercheckuser'@'%';\""
         result = host.run(cmd)
         assert result.rc == 0, result.stdout
-
+        cmd = 'service xinetd restart'
+        result = host.run(cmd)
+        assert result.rc == 0, result.stdout
+        cmd = 'service haproxy restart'
+        result = host.run(cmd)
+        assert result.rc == 0, result.stdout
+        time.sleep(60) 
 
 def test_haproxy_service(host):
     assert host.service("haproxy").is_running
@@ -26,7 +33,7 @@ def test_haproxy(host, create_user):
         cmd = "mysql -e \"SELECT VERSION();\""
         result = host.run(cmd)
         assert result.rc == 0, result.stdout
-        cmd = "mysql --port=9201 -e \"SELECT VERSION();\" "
+        cmd = "mysql --port=9201 -h127.0.0.1 -e \"SELECT VERSION();\" "
         result = host.run(cmd)
         print(result.stdout)
         assert result.rc == 0, result.stdout
