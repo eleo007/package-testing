54b57b2c17af1ec14d85898c99e1bfb79d399d58
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2024-03-01 20:31:25 +0200

    Use lists

--- binary-tarball-tests/ps/tests/test_ps_dynamic.py
+++ binary-tarball-tests/ps/tests/test_ps_dynamic.py
@@ -37,67 +37,65 @@ def test_fips_md5(host, mysql_server):
     else:
         pytest.skip("This test is only for PRO tarballs. Skipping")
 
-# def test_fips_value(host,mysql_server):
-#     dist = host.system_info.distribution
-#     major_version = version.parse(host.system_info.release).major
-#     if pro and fips_supported_os[dist] == major_version:
-#         query="select @@ssl_fips_mode;"
-#         output = mysql_server.run_query(query, True)
-#         assert 'ON' in output
-#     else:
-#         pytest.skip("This test is only for PRO tarballs. Skipping")
+def test_fips_value(host,mysql_server):
+    fips_supported=check_fips_compliance
+    if fips_supported:
+        query="select @@ssl_fips_mode;"
+        output = mysql_server.run_query(query, True)
+        assert 'ON' in output
+    else:
+        pytest.skip("This test is only for PRO tarballs. Skipping")
 
 
-# def test_fips_in_log(host, mysql_server):
-#     dist = host.system_info.distribution
-#     major_version = version.parse(host.system_info.release).major
-#     if pro and fips_supported_os[dist] == major_version:
-#         with host.sudo():
-#             query="SELECT @@log_error;"
-#             error_log = mysql_server.run_query(query, True)
-#             logs=host.check_output(f'head -n30 {error_log}')
-#             assert "A FIPS-approved version of the OpenSSL cryptographic library has been detected in the operating system with a properly configured FIPS module available for loading. Percona Server for MySQL will load this module and run in FIPS mode." in logs
-#     else:
-#         pytest.skip("This test is only for PRO tarballs. Skipping")
+def test_fips_in_log(host, mysql_server):
+    fips_supported=check_fips_compliance
+    if fips_supported:
+        with host.sudo():
+            query="SELECT @@log_error;"
+            error_log = mysql_server.run_query(query, True)
+            logs=host.check_output(f'head -n30 {error_log}')
+            assert "A FIPS-approved version of the OpenSSL cryptographic library has been detected in the operating system with a properly configured FIPS module available for loading. Percona Server for MySQL will load this module and run in FIPS mode." in logs
+    else:
+        pytest.skip("This test is only for PRO tarballs. Skipping")
 
-# def test_rocksdb_install(host, mysql_server):
-#     if ps_version_major not in ['5.6']:
-#         host.run(mysql_server.psadmin+' --user=root -S'+mysql_server.socket+' --enable-rocksdb')
-#         assert mysql_server.check_engine_active('ROCKSDB')
-#     else:
-#         pytest.skip('RocksDB is available from 5.7!')
+def test_rocksdb_install(host, mysql_server):
+    if ps_version_major not in ['5.6']:
+        host.run(mysql_server.psadmin+' --user=root -S'+mysql_server.socket+' --enable-rocksdb')
+        assert mysql_server.check_engine_active('ROCKSDB')
+    else:
+        pytest.skip('RocksDB is available from 5.7!')
 
-# def test_tokudb_install(host, mysql_server):
-#     if ps_version_major in ['5.6']:
-#         host.run('sudo '+mysql_server.psadmin+' --user=root -S'+mysql_server.socket+' --enable --enable-backup')
-#         mysql_server.restart()
-#         host.run('sudo '+mysql_server.psadmin+' --user=root -S'+mysql_server.socket+' --enable --enable-backup')
-#         assert mysql_server.check_engine_active('TokuDB')
-#     else:
-#         pytest.skip('TokuDB is skipped from 5.7!')
+def test_tokudb_install(host, mysql_server):
+    if ps_version_major in ['5.6']:
+        host.run('sudo '+mysql_server.psadmin+' --user=root -S'+mysql_server.socket+' --enable --enable-backup')
+        mysql_server.restart()
+        host.run('sudo '+mysql_server.psadmin+' --user=root -S'+mysql_server.socket+' --enable --enable-backup')
+        assert mysql_server.check_engine_active('TokuDB')
+    else:
+        pytest.skip('TokuDB is skipped from 5.7!')
 
-# def test_install_functions(mysql_server):
-#     for function in ps_functions:
-#         mysql_server.install_function(function[0], function[1], function[2])
+def test_install_functions(mysql_server):
+    for function in ps_functions:
+        mysql_server.install_function(function[0], function[1], function[2])
 
-# def test_install_component(mysql_server):
-#     if ps_version_major == '8.0' or re.match(r'^8\.[1-9]$', ps_version_major):
-#         for component in ps_components:
-#             mysql_server.install_component(component)
-#     else:
-#         pytest.skip('Component is checked from 8.0!')
+def test_install_component(mysql_server):
+    if ps_version_major == '8.0' or re.match(r'^8\.[1-9]$', ps_version_major):
+        for component in ps_components:
+            mysql_server.install_component(component)
+    else:
+        pytest.skip('Component is checked from 8.0!')
 
-# def test_install_plugin(mysql_server):
-#     for plugin in ps_plugins:
-#         mysql_server.install_plugin(plugin[0], plugin[1])
+def test_install_plugin(mysql_server):
+    for plugin in ps_plugins:
+        mysql_server.install_plugin(plugin[0], plugin[1])
 
-# def test_audit_log_v2(mysql_server):
-#     if ps_version_major in ['8.0']:
-#         query='source {}/share/audit_log_filter_linux_install.sql;'.format(base_dir)
-#         mysql_server.run_query(query)
-#         query = 'SELECT plugin_status FROM information_schema.plugins WHERE plugin_name = "audit_log_filter";'
-#         output = mysql_server.run_query(query)
-#         assert 'ACTIVE' in output
-#     else:
-#         pytest.skip('audit_log_v2 is checked from 8.0!')
+def test_audit_log_v2(mysql_server):
+    if ps_version_major in ['8.0']:
+        query='source {}/share/audit_log_filter_linux_install.sql;'.format(base_dir)
+        mysql_server.run_query(query)
+        query = 'SELECT plugin_status FROM information_schema.plugins WHERE plugin_name = "audit_log_filter";'
+        output = mysql_server.run_query(query)
+        assert 'ACTIVE' in output
+    else:
+        pytest.skip('audit_log_v2 is checked from 8.0!')
 
