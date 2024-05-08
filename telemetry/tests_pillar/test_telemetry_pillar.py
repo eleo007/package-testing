4efd3a6e48389a77c0fb7bd95d164c205ab1bf27
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2024-05-08 22:21:32 +0300

    Add removal checkss

--- telemetry/tests_pillar/test_telemetry_pillar.py
+++ telemetry/tests_pillar/test_telemetry_pillar.py
@@ -571,61 +571,61 @@ def test_disable_service(host):
 ############# REMOVAL TESTS #############
 #########################################
 
-# def test_telem_path_not_writtable(host):
-#     with host.sudo("root"):
-#         log_file=host.check_output('mysql -u root -Ns -e \'select @@log_error;\'')
-#         update_ps_options(host, '20', '10')
-#         host.check_output(f'chown root:root {ps_pillar_dir}')
-#         time.sleep(30)
-#         log_file_content = host.file(log_file).content_string
-#         mysql_serv = host.service("mysql")
-#         assert "Problem during telemetry file write: Permission denied" in log_file_content
-#         assert mysql_serv.is_running
-#         host.check_output(f'chown mysql:percona-telemetry {ps_pillar_dir}')
+def test_telem_path_not_writtable(host):
+    with host.sudo("root"):
+        log_file=host.check_output('mysql -u root -Ns -e \'select @@log_error;\'')
+        update_ps_options(host, '20', '10')
+        host.check_output(f'chown root:root {ps_pillar_dir}')
+        time.sleep(30)
+        log_file_content = host.file(log_file).content_string
+        mysql_serv = host.service("mysql")
+        assert "Problem during telemetry file write: Permission denied" in log_file_content
+        assert mysql_serv.is_running
+        host.check_output(f'chown mysql:percona-telemetry {ps_pillar_dir}')
 
-# def test_telem_path_absent(host):
-#     with host.sudo("root"):
-#         log_file=host.check_output('mysql -u root -Ns -e \'select @@log_error;\'')
-#         update_ps_options(host, '20', '10')
-#         cmd = f'rm -rf {ps_pillar_dir}'
-#         host.check_output(cmd)
-#         time.sleep(30)
-#         log_file_content = host.file(log_file).content_string
-#         mysql_serv = host.service("mysql")
-#         assert "Component percona_telemetry reported: 'Problem during telemetry file write: filesystem error: directory iterator cannot open directory: No such file or directory [/usr/local/percona/telemetry/ps]" in log_file_content
-#         assert mysql_serv.is_running
+def test_telem_path_absent(host):
+    with host.sudo("root"):
+        log_file=host.check_output('mysql -u root -Ns -e \'select @@log_error;\'')
+        update_ps_options(host, '20', '10')
+        cmd = f'rm -rf {ps_pillar_dir}'
+        host.check_output(cmd)
+        time.sleep(30)
+        log_file_content = host.file(log_file).content_string
+        mysql_serv = host.service("mysql")
+        assert "Component percona_telemetry reported: 'Problem during telemetry file write: filesystem error: directory iterator cannot open directory: No such file or directory [/usr/local/percona/telemetry/ps]" in log_file_content
+        assert mysql_serv.is_running
 
-# def test_path_absent_after_removal(host):
-#     dist = host.system_info.distribution
-#     if dist.lower() in DEB_DISTS:
-#         host.check_output("apt autoremove -y percona-server-server")
-#     else:
-#         host.check_output("yum remove -y percona-server-server")
-#     assert not host.file(ps_pillar_dir).exists
+def test_path_absent_after_removal(host):
+    dist = host.system_info.distribution
+    if dist.lower() in DEB_DISTS:
+        host.check_output("apt autoremove -y percona-server-server")
+    else:
+        host.check_output("yum remove -y percona-server-server")
+    assert not host.file(ps_pillar_dir).exists
 
-# def test_ta_service_removed_deb(host):
-#     dist = host.system_info.distribution
-#     if dist.lower() not in DEB_DISTS:
-#         pytest.skip("This test only for DEB distributions")
-#     with host.sudo("root"):
-#         host.run("systemctl daemon-reload")
-#         ta_serv_result = host.run("systemctl status percona-telemetry-agent").stderr
-#     assert "Unit percona-telemetry-agent.service could not be found." in ta_serv_result
-#     assert host.file(telem_history_dir).exists
+def test_ta_service_removed_deb(host):
+    dist = host.system_info.distribution
+    if dist.lower() not in DEB_DISTS:
+        pytest.skip("This test only for DEB distributions")
+    with host.sudo("root"):
+        host.run("systemctl daemon-reload")
+        ta_serv_result = host.run("systemctl status percona-telemetry-agent").stderr
+    assert "Unit percona-telemetry-agent.service could not be found." in ta_serv_result
+    assert host.file(telem_history_dir).exists
 
-# def test_ta_service_removed_rpm(host):
-#     dist = host.system_info.distribution
-#     if dist.lower() in DEB_DISTS:
-#         pytest.skip("This test only for RPM distributions")
-#     with host.sudo("root"):
-#         # https://perconadev.atlassian.net/browse/PKG-46
-#         host.check_output("systemctl daemon-reexec")
-#         ta_serv_result = host.run("systemctl status percona-telemetry-agent").stderr
-#     assert "Unit percona-telemetry-agent.service could not be found." in ta_serv_result
-#     assert host.file(telem_history_dir).exists
+def test_ta_service_removed_rpm(host):
+    dist = host.system_info.distribution
+    if dist.lower() in DEB_DISTS:
+        pytest.skip("This test only for RPM distributions")
+    with host.sudo("root"):
+        # https://perconadev.atlassian.net/browse/PKG-46
+        host.check_output("systemctl daemon-reexec")
+        ta_serv_result = host.run("systemctl status percona-telemetry-agent").stderr
+    assert "Unit percona-telemetry-agent.service could not be found." in ta_serv_result
+    assert host.file(telem_history_dir).exists
 
-# def test_ta_process_not_running(host):
-#     cmd = 'ps auxww| grep -v grep  | grep -c "percona-telemetry-agent"'
-#     result = host.run(cmd)
-#     stdout = int(result.stdout)
-#     assert stdout == 0
+def test_ta_process_not_running(host):
+    cmd = 'ps auxww| grep -v grep  | grep -c "percona-telemetry-agent"'
+    result = host.run(cmd)
+    stdout = int(result.stdout)
+    assert stdout == 0
